c2d5c54c5df6ba7b2bb0e5da78a1cace9ddf2711
KyraB <bosta024@d.umn.edu>
2026-09-22 15:13:20 -0500

    Added method in BubbleSort so timing would be self-contained within the BubbleSort class.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,4 +1,29 @@
 public class BubbleSort {
+    // timing method that takes the three arrays of the same size
+    public static void bubbleSortAndTime(int[] arr1, int[] arr2, int[] arr3, int size) {
+        // time first run
+        long startTime1 = System.nanoTime();
+        bubbleSort(arr1);
+        long endTime1 = System.nanoTime();
+        long executionTime1 = (endTime1 - startTime1) / 1000000;
+
+        // time second run
+        long startTime2 = System.nanoTime();
+        bubbleSort(arr2);
+        long endTime2 = System.nanoTime();
+        long executionTime2 = (endTime2 - startTime2) / 1000000;
+
+        // time third run
+        long startTime3 = System.nanoTime();
+        bubbleSort(arr3);
+        long endTime3 = System.nanoTime();
+        long executionTime3 = (endTime3 - startTime3) / 1000000;
+
+        // calculate and print average
+        long avgExecutionTime = (executionTime1 + executionTime2 + executionTime3) / 3;
+        System.out.println("Using bubble sort on an array size of " + size +
+                " takes on average " + avgExecutionTime + " ms");
+    }
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
--- src/Main.java
+++ src/Main.java
@@ -5,49 +5,30 @@ public class Main {
         int smallestArraySize = 100000;
         int mediumArraySize = 1000000;
         int largestArraySize = 10000000;
+
         int[] smallestRA1 = RandomArrayGenerator.generateRandomArray(smallestArraySize);
-
         int[] smallestRA2 = RandomArrayGenerator.generateRandomArray(smallestArraySize);
-
         int[] smallestRA3 = RandomArrayGenerator.generateRandomArray(smallestArraySize);
 
         int[] mediumRA1 = RandomArrayGenerator.generateRandomArray(mediumArraySize);
-
         int[] mediumRA2 = RandomArrayGenerator.generateRandomArray(mediumArraySize);
-
         int[] mediumRA3 = RandomArrayGenerator.generateRandomArray(mediumArraySize);
 
         int[] largestRA1 = RandomArrayGenerator.generateRandomArray(largestArraySize);
-
         int[] largestRA2 = RandomArrayGenerator.generateRandomArray(largestArraySize);
-
         int[] largestRA3 = RandomArrayGenerator.generateRandomArray(largestArraySize);
 
         int [] smallBubble1 = smallestRA1.clone();
-        // Start measuring execution time
-        long smallBubble1startTime = System.nanoTime();
-        BubbleSort.bubbleSort(smallBubble1);
-        // Stop measuring execution time
-        long smallBubble1endTime = System.nanoTime();
-        // Calculate the execution time in milliseconds
-        long smallBubble1ExecutionTime = (smallBubble1endTime - smallBubble1startTime) / 1000000;
-
         int [] smallBubble2 = smallestRA2.clone();
-        long smallBubble2startTime = System.nanoTime();
-        BubbleSort.bubbleSort(smallBubble2);
-        long smallBubble2endTime = System.nanoTime();
-        long smallBubble2ExecutionTime = (smallBubble2endTime - smallBubble2startTime) / 1000000;
-
         int [] smallBubble3 = smallestRA3.clone();
-        long smallBubble3startTime = System.nanoTime();
-        BubbleSort.bubbleSort(smallBubble3);
-        long smallBubble3endTime = System.nanoTime();
-        long smallBubble3ExecutionTime = (smallBubble3endTime - smallBubble3startTime) / 1000000;
 
-        long avgSmallBubbleExecutionTime = (smallBubble1ExecutionTime + smallBubble2ExecutionTime +
-                smallBubble3ExecutionTime) / 3;
+        BubbleSort.bubbleSortAndTime(smallBubble1, smallBubble2, smallBubble3, smallestArraySize);
 
-        System.out.println("Using bubble sort on an array size of " + smallestArraySize +
-                " takes on average " + avgSmallBubbleExecutionTime + " ms");
+        int [] mediumBubble1 = mediumRA1.clone();
+        int [] mediumBubble2 = mediumRA2.clone();
+        int [] mediumBubble3 = mediumRA3.clone();
+
+        BubbleSort.bubbleSortAndTime(mediumBubble1, mediumBubble2, mediumBubble3, mediumArraySize);
+
     }
 }

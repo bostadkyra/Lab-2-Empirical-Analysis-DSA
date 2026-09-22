32c78b1b78d327b76d12492f1183fcc3cee45e50
KyraB <bosta024@d.umn.edu>
2026-09-22 18:23:56 -0500

    Adjusted sizes of arrays so the sort methods would run

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int smallestArraySize = 100000;
-        int mediumArraySize = 1000000;
-        int largestArraySize = 10000000;
+        int smallestArraySize = 10000;
+        int mediumArraySize = 100000;
+        int largestArraySize = 1000000;
 
         int[] smallestRA1 = RandomArrayGenerator.generateRandomArray(smallestArraySize);
         int[] smallestRA2 = RandomArrayGenerator.generateRandomArray(smallestArraySize);
@@ -22,13 +22,19 @@ public class Main {
         int [] smallBubble2 = smallestRA2.clone();
         int [] smallBubble3 = smallestRA3.clone();
 
-        BubbleSort.bubbleSortAndTime(smallBubble1, smallBubble2, smallBubble3, smallestArraySize);
+        //BubbleSort.bubbleSortAndTime(smallBubble1, smallBubble2, smallBubble3, smallestArraySize);
 
         int [] mediumBubble1 = mediumRA1.clone();
         int [] mediumBubble2 = mediumRA2.clone();
         int [] mediumBubble3 = mediumRA3.clone();
 
-        BubbleSort.bubbleSortAndTime(mediumBubble1, mediumBubble2, mediumBubble3, mediumArraySize);
+        //BubbleSort.bubbleSortAndTime(mediumBubble1, mediumBubble2, mediumBubble3, mediumArraySize);
+
+        int [] largeBubble1 = largestRA1.clone();
+        int [] largeBubble2 = largestRA2.clone();
+        int [] largeBubble3 = largestRA3.clone();
+
+        BubbleSort.bubbleSortAndTime(largeBubble1, largeBubble2, largeBubble3, largestArraySize);
 
     }
 }
